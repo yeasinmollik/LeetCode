1d2289507b3a8131bf955987be45037e8a88709a
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-03-07 10:44:02 +0600

    added solution

---
+++ 3. Longest Substring Without Repeating Characters/3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        if (n == 0)
+            return 0;
+
+        vector<int> lastIndex(128, -1);
+        lastIndex[s[0]] = 0;
+
+        int prevLongest = 1, ans = 1, currLongest;
+
+        for (int i = 1; i < n; i++) {
+            currLongest = min(prevLongest + 1, i - lastIndex[s[i]]);
+            ans = max(ans, currLongest);
+
+            prevLongest = currLongest;
+            lastIndex[s[i]] = i;
+        }
+
+        return ans;
+    }
+};
