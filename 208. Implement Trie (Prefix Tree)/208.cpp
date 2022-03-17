d9d7180f273ded2d347c229b569402f78b2bd5e3
Yaseen Mollik <yeasin1603@cseku.ac.bd>
2022-03-17 17:36:40 +0600

    added solutiom

---
+++ 208. Implement Trie (Prefix Tree)/208.cpp
@@ -0,0 +1,51 @@
+/*
+    Author: Yeasin Mollik
+    Problem Name: Implement Trie (Prefix Tree)
+    Date: 6/3/2022
+*/
+
+class Trie {
+   public:
+    Trie() {
+        root = new Node();
+    }
+
+    struct Node {
+        Node *child[26];
+        bool isEnd = false;
+    } * root;
+
+    void insert(string word) {
+        auto curr = root;
+        for (char ch : word) {
+            int c = ch - 'a';
+            if (curr->child[c] == NULL)
+                curr->child[c] = new Node();
+            curr = curr->child[c];
+        }
+        curr->isEnd = true;
+    }
+
+    bool search(string word) {
+        auto curr = root;
+        for (char ch : word) {
+            int c = ch - 'a';
+            if (curr->child[c] == NULL)
+                return false;
+            curr = curr->child[c];
+        }
+        return curr->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+        auto curr = root;
+        for (char ch : prefix) {
+            int c = ch - 'a';
+            if (curr->child[c] == NULL)
+                return false;
+            curr = curr->child[c];
+        }
+        return true;
+    }
+};
+
